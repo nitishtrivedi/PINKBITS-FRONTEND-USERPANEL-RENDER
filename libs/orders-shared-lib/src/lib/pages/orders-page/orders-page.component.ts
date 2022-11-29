209d7593387d802de670ca6ac7eb2455f3d71baa
nitishtrivedi <nitish_trivedi0194@yahoo.com>
2022-11-29 15:50:19 +0530

    Changes Updated

--- libs/orders-shared-lib/src/lib/pages/orders-page/orders-page.component.ts
+++ libs/orders-shared-lib/src/lib/pages/orders-page/orders-page.component.ts
@@ -9,11 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdersPageComponent implements OnInit {
     //GV
-    cars: 'Car[]';
 
     constructor() {}
 
     ngOnInit(): void {
-        this.cars;
     }
 }
